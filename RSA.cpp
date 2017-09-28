b9b9962a175e94a6d97cfa2676c7f054a5ac564a
manoj2527 <30951638+manoj2527@users.noreply.github.com>
2017-09-28 23:41:11 +0530

    Add files via upload

---
+++ RSA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "bigint.h"
+#include <utility>
+using namespace std;
+
+bigint encryption(bigint m,bigint e,bigint n)
+{
+	return exp(m,e,n);
+}
+
+
+bigint decryption(bigint c,bigint e,bigint p,bigint q)
+{
+	bigint d=inverse(e,(p-1)*(q-1));
+	return exp(c,e,p*q);
+}
+
